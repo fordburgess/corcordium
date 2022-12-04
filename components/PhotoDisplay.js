5d508a51c3874eb851d15cfd51c9a84e1d34e016
ford burgess <fburg6824@gmail.com>
2022-12-04 14:24:05 +0000

    homepage photo gallery is now separate component

---
+++ components/PhotoDisplay.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './photodisplay.module.css'
+import Angela1 from '../media/angela1.jpg'
+import Angela2 from '../media/angela2.jpg'
+import Angela3 from '../media/angela3.jpg'
+import Angela4 from '../media/angela4.jpg'
+import Angela5 from '../media/angela5.jpg'
+
+
+const PhotoDisplay = () => {
+  return (
+    <div className={styles.container}>
+      <h4 className={styles.title}>
+        Portfolio
+      </h4>
+      <div className={styles.doublePhotoContainer}>
+        <Image className={styles.smallImage} src={Angela1} alt="angela" />
+        <Image className={styles.smallImage} src={Angela2} alt="angela2" />
+      </div>
+      <div className={styles.singlePhotoContainer}>
+        <Image className={styles.largeImage} src={Angela3} alt="angela3" />
+      </div>
+      <div className={styles.doublePhotoContainer} style={{marginBottom: "30px"}}>
+        <Image className={styles.smallImage} src={Angela4} alt="angela4"/>
+        <Image className={styles.smallImage} src={Angela5} alt="angela5"/>
+      </div>
+      <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
+        <a href="/gallery" className={styles.galleryLink}>Discover More</a>
+      </div>
+    </div>
+  )
+}
+
+export default PhotoDisplay
