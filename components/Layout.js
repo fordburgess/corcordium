f0f8c80a68f27cbc3b38a0615f846c5b6a568b8a
ford burgess <fburg6824@gmail.com>
2022-12-04 15:25:39 +0000

    desktop hero halfway finished

--- components/Layout.js
+++ components/Layout.js
@@ -5,19 +5,19 @@ import Footer from './Footer';
 
 export default function Layout({ children }) {
 
-  return (
-    <>
-      <style jsx global>
-        {`
-        body {
-          margin: 0;
-          padding: 0;
-        }
-      `}
-      </style>
-      <Navbar />
-        <main>{children}</main>
-      <Footer />
-    </>
-  );
+    return (
+      <>
+        <style jsx global>
+          {`
+          body {
+            margin: 0;
+            padding: 0;
+          }
+        `}
+        </style>
+        <Navbar />
+          <main>{children}</main>
+        <Footer />
+      </>
+    )
 }
