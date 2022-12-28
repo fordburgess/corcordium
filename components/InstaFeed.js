88e71fd60261fc4c1f8290d02e5909b605d40837
ford burgess <fburg6824@gmail.com>
2022-12-27 19:58:30 -0600

    removed mui

--- components/InstaFeed.js
+++ components/InstaFeed.js
@@ -5,48 +5,6 @@ import styles from './instafeed.module.css';
 const InstaFeed = () => {
   const [posts, setPosts] = useState([]);
 
-  const instaId = "56382033902";
-  const thumbnailWidth = 480;
-  const photoCount = 5;
-
-//   useEffect(() => {
-//     const getPosts = async () => {
-//       const options = {
-//         method: 'GET',
-//         mode: 'no-cors',
-//       };
-//       const res = await fetch(
-//         ``
-//       );
-//
-//       const { data } = await res.json();
-//       const photos = data.user.edge_owner_to_timeline_media.edges.map(
-//         ({ node }) => {
-//           const id = { node };
-//           const likes = node.edge_media_preview_like.count;
-//           const caption = node.edge_media_to_caption.edges[0].node.text;
-//           const thumbnail = node.thumbnail_resources.find(
-//             thumbnail => thumbnail.config_width === thumbnailWidth
-//           )
-//           const { src, config_width: width, config_height: height } = thumbnail
-//           const url = `https://www.instagram.com/p/${node.shortcode}`
-//           return {
-//             id,
-//             caption,
-//             src,
-//             width,
-//             height,
-//             url,
-//             comments,
-//             likes
-//           }
-//         }
-//       )
-//       setPosts(prev => [...prev, photos]);
-//     }
-//
-//     getPosts();
-//   }, []);
 
   return (
     <div className={styles.container}>
