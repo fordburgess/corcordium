c99c7030c45e31f3b65ef204843873b8cfdf118a
ford burgess <fburg6824@gmail.com>
2023-01-03 00:35:07 -0600

    will use dynamic webpage instead

--- pages/portfolio/gallery.js
+++ pages/portfolio/gallery.js
@@ -3,33 +3,15 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '../../media/logo1.png'
 import styles from './gallery.module.css';
-import ImageDisplay from '../../utils/ImageDisplay';
+import Projects from './projects.json';
 
-function Gallery() {
+function Gallery({ photos }) {
+  const [activeProject, setActiveProject] = useState("all")
+  console.log(activeProject)
+  var test = [...photos[0]];
 
-  const test = {
-    gkey: "AIzaSyDD_Yz-KLtpid3HwZXpofJ_QaZgCsKhh58",
-    dirId: "1hQD4Td-gsLD1r-2StKLrSck2jlOIbW83",
-    name: "CorCordium",
-    options: {
-      style: {
-      },
-      onClick: {
-          modal: true,
-          newWindow: false
-      },
-      exclude: {
-          "1.jpg": true
-      },
-      attachClass: {
-          "Angela": "angela"
-      },
-      attachId: {
-          "2.jpg": "test2"
-      },
-      hover: true
-    }
-  }
+    var allImages = true;
+
 
   return (
     <>
@@ -38,12 +20,63 @@ function Gallery() {
           <Link href="/"><Image src={Logo} alt="logo" className={styles.logo}/></Link>
           <h3>Portfolio</h3>
         </div>
-          <div className={styles.imageContainer}>
-            <ImageDisplay data={test}/>
-          </div>
+        {activeProject == "all" ? (
+            <div className={styles.imageContainer}>
+              {test.map(item => {
+                if (activeProject !== "all" && !item.title.includes(activeProject)) {
+                  allImages = false;
+                }
+                let imgUrl = "http://drive.google.com/uc?export=view&id=" + item.id;
+                var name = item.title.split(/[0-9]/)[0];
+                return (
+                  <div key={item.title} className={styles.contentContainer}>
+                    <img src={imgUrl} alt="portfolio" key={item.id} className={styles.image}/>
+                    <div className={styles.info}>
+                      <h1>{name}</h1>
+                      <p className={styles.readMore} onClick={() => setActiveProject(name)}>Read More</p>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+        ) : (
+            <div className={styles.projectContainer}>
+              <div className={styles.projectPhotoContainer}>
+                {test.map(item => {
+                  let imgUrl = "http://drive.google.com/uc?export=view&id=" + item.id;
+                  if (item.title.includes(activeProject)) {
+                    return (
+                      <div key={item.title} className={styles.contentContainer}>
+                        <img src={imgUrl} alt="portfolio" key={item.id} className={styles.image}/>
+                      </div>
+                    )
+                  }
+                })}
+              </div>
+              <div className={styles.projectContentContainer}>
+                <h1>{activeProject}</h1>
+                {/* <h6>{project.text}</h6> */}
+              </div>
+            </div>
+          )}
       </div>
     </>
   )
+}
+
+Gallery.getInitialProps = async (ctx) => {
+  const imgIds = [];
+
+  const URL_START = "https://www.googleapis.com/drive/v2/files?q=%27";
+  const URL_END = "%27+in+parents&key=";
+
+  await fetch(URL_START + process.env.NEXT_PUBLIC_DIR_ID + URL_END + process.env.NEXT_PUBLIC_G_KEY)
+    .then(res => res.json())
+    .then(jsonRes => imgIds.push(jsonRes.items));
+
+  return {
+    photos: imgIds,
+  }
 }
 
 
