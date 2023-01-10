d078fe3eb07e2a0413e0a9ce2b6126de0de4b371
ford burgess <fburg6824@gmail.com>
2023-01-10 17:25:02 -0600

    made logo responsive

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>Corcordium</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://freepngdownload.com/image/thumb/heart-png-free-image-download-1.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <PhotoDisplay />
       <LatestArticles />
