c0b133ecb0b5a7e1f3582fa7c3caf1e95217f1eb
ford burgess <fburg6824@gmail.com>
2023-01-14 20:00:42 -0600

    completed mobile insta feed(?)

--- components/InstaFeed.js
+++ components/InstaFeed.js
@@ -1,21 +1,53 @@
+import { flexbox } from '@mui/system';
+import Link from 'next/link'
 import React, { useState, useEffect } from 'react';
-import Image from 'next/image'
+
 import styles from './instafeed.module.css';
+
+
 
 const InstaFeed = () => {
   const [posts, setPosts] = useState([]);
+  const [post1, setPost1] = useState([])
+  const [post2, setPost2] = useState('');
+  const [post3, setPost3] = useState('');
+
+  useEffect(() => {
+
+    async function fetchPosts() {
+      await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,thumbnail_url,timestamp,username&access_token=${process.env.NEXT_PUBLIC_INSTA_TOKEN}`)
+      .then(res => res.json())
+      .then(res => {
+        setPosts(res.data)
+        setPost1(res.data[0].media_url)
+        setPost2(res.data[1].media_url)
+        setPost3(res.data[2].media_url)
+      });
+
+    }
+    fetchPosts();
+  }, [])
+
+
+  const link = "https://www.instagram.com/corcordium.archive/"
 
 
 
   return (
     <div className={styles.container}>
       <div className={styles.subContainer}>
-        <h3 className={styles.header}>For everyday fashion stories</h3>
-        {posts.map(({ src, url, id, caption, likes, comments }) => {
-            <div className={styles.post} key={id}>
-              <Image src={src} alt="post" className={styles.postImage}/>
-            </div>
-        })}
+        <h4 className={styles.header}>For more fashion stories</h4>
+        <Link href={link} style={{ color: "#000000" }}><h2 className={styles.header}>@corcordium.archive</h2></Link>
+        <div className={styles.mobileFeed}>
+          <div style={{ backgroundImage: `url(${post1})`, height: "100%", width: "50%", backgroundSize: "cover"}}></div>
+          <div style={{ display: "flex", flexDirection: "column", width: "50%"}}>
+            <img src={post2} style={{ height: "50%", width: "100%", objectFit: "cover"}} />
+            <img src={post3} style={{ height: "50%", width: "100%", objectFit: "cover"}} />
+          </div>
+        </div>
+        <div className={styles.desktopFeed}>
+
+        </div>
       </div>
     </div>
   )
