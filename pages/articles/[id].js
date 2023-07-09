3a42301b8700523940f9262170147b1d2ef51575
ford burgess <fburg6824@gmail.com>
2023-07-09 13:57:37 +0100

    hopefully the photos work

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -60,12 +60,12 @@ const options = {
     [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
       // render the EMBEDDED_ASSET as you need
       return (
-        <Image
+        <img
           src={`https://${node.data.target.fields.file.url}`}
           height={node.data.target.fields.file.details.image.height}
-          width={node.data.target.fields.file.details.image.width}
+          width={node.data.target.fields.file.details.image.width / 2}
           alt={node.data.target.fields.description}
-          // style={{objectFit: "cover", objectPosition: "center"}}
+          style={{objectFit: "cover", objectPosition: "center"}}
         />
       );
     }
