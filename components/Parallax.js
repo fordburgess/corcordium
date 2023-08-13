9efb4e6205d9a3049d3113bfba2aa50228ad70a7
ford burgess <fburg6824@gmail.com>
2023-08-13 17:05:43 +0100

    front page reorganized

---
+++ components/Parallax.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
+import style from "./Parallax.module.css"
+import cx from 'classnames'
+import angela6 from "../media/angela6.jpg"
+
+const Parallax = () => {
+  const [isGrowing, setIsGrowing] = useState(false)
+  const [fadeInLeft, setFadeInLeft] = useState(false);
+  const [fadeInRight, setFadeInRight] = useState(false);
+  const [fadeInBottom, setFadeInBottom] = useState(false);
+
+  useEffect(() => {
+    // setIsGrowing(true);
+    setFadeInLeft(true);
+    setTimeout(() => {
+      setFadeInRight(true);
+    }, 500);
+    setTimeout(() => {
+      setFadeInBottom(true);
+    }, 1000);
+  }, [])
+
+  return (
+    <div className={style.container}>
+      <div className={style.topSubContainer}>
+        <h1 className={cx(style.title, fadeInLeft && style.fadeInLeft)}>CorCordium</h1>
+        <div style={{ height: "300px" }}>
+          <svg width="3" height="300" className={cx(style.svg, isGrowing && style.grow)}>
+            <line x1="1.5" y1="0" x2="1.5" y2="300" stroke="black" stroke-width="3" />
+          </svg>
+        </div>
+        <div className={cx(style.topSubContainerRight, fadeInRight && style.fadeInRight)}>
+          <h1>Leelou</h1>
+          <h1>Reboh</h1>
+        </div>
+      </div>
+      <div className={cx(style.photoPlayer, fadeInBottom && style.fadeInBottom)}>
+        <Image src={angela6} style={{ height: "100%", width: "100%" }} alt="angela"/>
+      </div>
+    </div>
+  )
+}
+
+export default Parallax
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image'
 import styles from './index.module.css';
+import Parallax from '../components/Parallax';
 import PhotoDisplay from '../components/PhotoDisplay';
 import LatestArticles from '../components/LatestArticles';
 import InstaFeed from '../components/InstaFeed';
@@ -15,7 +16,8 @@ export default function Home({ feed, latestArticles }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PhotoDisplay />
+      <Parallax />
+      {/* <PhotoDisplay /> */}
       <LatestArticles articles={latestArticles}/>
       <InstaFeed posts={feed}/>
     </div>
