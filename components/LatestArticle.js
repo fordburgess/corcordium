e96dfab7642673fb0653dff021dbf006bdf13f31
ford burgess <fburg6824@gmail.com>
2023-09-03 16:20:52 +0100

    latest articles complete for desktop

--- components/LatestArticle.js
+++ components/LatestArticle.js
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router';
 import styles from "./latestarticle.module.css"
 import dateFormat, { masks } from "dateformat";
+import cx from "classnames"
+import { style } from '@mui/system';
 
 const LatestArticle = (props) => {
-  const router = useRouter();
+  const [isVisible, setIsVisible] = useState(false);
+  const imageRef = useRef(null)
 
   var path = `/articles/${props.id}`
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (imageRef.current) {
+        const rect = imageRef.current.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (rect.top <= windowHeight * 0.75 && rect.bottom > 0) {
+          setIsVisible(true);
+        }
+        else {
+          setIsVisible(false);
+        }
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [])
 
   return (
     <div className={styles.container}>
       <Link href={path} className={styles.link}>
-        <img src={props.image} alt="article photo" className={styles.image}/>
-        <p className={styles.date}>{dateFormat(Date.parse(props.date), "dd/mm/yyyy")}</p>
-        <div style={{width: "100%", marginTop: "-10px"}}>
-          <h3 className={styles.title}>{props.title}</h3>
+        <img src={props.image} alt="article photo" className={cx(styles.image, isVisible && styles.visible)} ref={imageRef} />
+        <div className={cx(styles.textContainer, isVisible && styles.visible)}>
+          <div className={styles.titleContainer}>
+            <h3 className={styles.title}>{props.title}</h3>
+            <p className={styles.date}>{dateFormat(Date.parse(props.date), "dd/mm/yyyy")}</p>
+          </div>
+          <p className={styles.blurb}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
         </div>
       </Link>
     </div>
--- components/Parallax.js
+++ components/Parallax.js
@@ -3,11 +3,10 @@ import Image from 'next/image'
 import style from "./Parallax.module.css"
 import cx from 'classnames'
 import angela6 from "../media/angela6.jpg"
-import { Scrollama, Step } from "scrollama"
+import scrollama from 'scrollama'
 
 const Parallax = () => {
-  const [scrollValue, setScrollValue] = useState(0)
-  const [isGrowing, setIsGrowing] = useState(false)
+  const [stickyPhoto, setStickyPhoto] = useState(false)
   const [fadeInLeft, setFadeInLeft] = useState(false);
   const [fadeInRight, setFadeInRight] = useState(false);
   const [fadeInBottom, setFadeInBottom] = useState(false);
@@ -22,9 +21,27 @@ const Parallax = () => {
     }, 1000);
   }, [])
 
-  const handleStepEnter = ({ element, index, direction }) => {
-    setScrollValue(index)
-  }
+
+  useEffect(() => {
+    const scroller = scrollama();
+
+    const handleStepEnter = ({element, index, direction}) => {
+      console.log("hello")
+      if (direction === 'down') {
+        setStickyPhoto(true);
+      } else {
+        setStickyPhoto(false);
+      }
+    }
+
+    scroller
+      .setup({
+        step: ".photoPlayer",
+
+      })
+      .onStepEnter(handleStepEnter)
+
+  }, [])
 
   return (
     <div className={style.container}>
@@ -32,24 +49,22 @@ const Parallax = () => {
         <div className={style.topSubContainer}>
           <h1 className={cx(style.title, fadeInLeft && style.fadeInLeft)}>CorCordium</h1>
           <div style={{ height: "300px" }}>
-            <svg width="3" height="300" className={cx(style.svg, isGrowing && style.grow)}>
-              <line x1="1.5" y1="0" x2="1.5" y2="300" stroke="black" stroke-width="3" />
+            <svg width="3" height="300" className={style.svg}>
+              <line x1="1.5" y1="0" x2="1.5" y2="300" stroke="black" strokeWidth="3" />
             </svg>
           </div>
           <div className={cx(style.topSubContainerRight, fadeInRight && style.fadeInRight)}>
             <h1>Leelou</h1>
             <h1>Reboh</h1>
-            <p>Fashion Girlie™</p>
+            {/* <p>Fashion Girlie™</p> */}
           </div>
         </div>
 
 
-      <div className={cx(style.photoPlayer, fadeInBottom && style.fadeInBottom)}>
+      <div className={cx(style.photoPlayer, fadeInBottom && style.fadeInBottom, stickyPhoto && style.sticky)}>
         <Image src={angela6} style={{ height: "100%", width: "100%" }} alt="angela"/>
       </div>
-      {/* <Scrollama onStepEnter={handleStepEnter}>
 
-      </Scrollama> */}
     </div>
   )
 }
