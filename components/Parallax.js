0a5a48ef68e82d3da75a9b78bbe55f8e3c21e7be
ford burgess <fburg6824@gmail.com>
2023-09-10 18:20:47 +0100

    added another photo

--- components/Parallax.js
+++ components/Parallax.js
@@ -24,6 +24,7 @@ const Parallax = () => {
     nora3ref: useRef(),
     angela2ref: useRef(),
     angela3ref: useRef(),
+    nora4ref: useRef()
   }
 
   const handleScroll = () => {
@@ -57,6 +58,7 @@ const Parallax = () => {
       mobilePhotoRefs.angela6ref.current.style.zIndex = 0;
       mobilePhotoRefs.angela2ref.current.style.zIndex = 0;
       mobilePhotoRefs.angela3ref.current.style.zIndex = 0;
+      mobilePhotoRefs.nora4ref.current.style.zIndex = 0;
     }
     if (scrollPosition >= 100 && scrollPosition <= 200) {
       mobilePhotoRefs.angela6ref.current.style.zIndex = 1;
@@ -68,34 +70,39 @@ const Parallax = () => {
       mobilePhotoRefs.angela6ref.current.style.zIndex = 0;
       mobilePhotoRefs.nora2ref.current.style.zIndex = 0;
     }
-    if (scrollPosition > 275 && scrollPosition <= 325) {
+    if (scrollPosition > 275 && scrollPosition <= 340) {
       mobilePhotoRefs.nora2ref.current.style.zIndex = 1;
       mobilePhotoRefs.kim2ref.current.style.zIndex = 0;
       mobilePhotoRefs.nora1ref.current.style.zIndex = 0;
     }
-    if (scrollPosition > 325 && scrollPosition <= 375) {
+    if (scrollPosition > 340 && scrollPosition <= 400) {
       mobilePhotoRefs.nora1ref.current.style.zIndex = 1;
       mobilePhotoRefs.nora2ref.current.style.zIndex = 0;
       mobilePhotoRefs.angela5ref.current.style.zIndex = 0;
     }
-    if (scrollPosition > 375 && scrollPosition <= 400) {
+    if (scrollPosition > 400 && scrollPosition <= 440) {
       mobilePhotoRefs.angela5ref.current.style.zIndex = 1;
       mobilePhotoRefs.nora1ref.current.style.zIndex = 0;
       mobilePhotoRefs.kim1ref.current.style.zIndex = 0;
     }
-    if (scrollPosition > 400 && scrollPosition <= 420) {
+    if (scrollPosition > 440 && scrollPosition <= 480) {
       mobilePhotoRefs.kim1ref.current.style.zIndex = 1;
       mobilePhotoRefs.angela5ref.current.style.zIndex = 0;
       mobilePhotoRefs.angela2ref.current.style.zIndex = 0;
     }
-    if (scrollPosition > 420 && scrollPosition <= 435) {
+    if (scrollPosition > 480 && scrollPosition <= 510) {
       mobilePhotoRefs.angela2ref.current.style.zIndex = 1;
       mobilePhotoRefs.kim1ref.current.style.zIndex = 0;
       mobilePhotoRefs.angela3ref.current.style.zIndex = 0;
     }
-    if (scrollPosition > 435) {
-      mobilePhotoRefs.angela3ref.current.style.zIndex = 1;
+    if (scrollPosition > 510 && scrollPosition <= 535) {
+      mobilePhotoRefs.nora4ref.current.style.zIndex = 1;
       mobilePhotoRefs.angela2ref.current.style.zIndex = 0;
+      mobilePhotoRefs.angela3ref.current.style.zIndex = 0;
+    }
+    if (scrollPosition > 535) {
+      mobilePhotoRefs.angela3ref.current.style.zIndex = 1;
+      mobilePhotoRefs.nora4ref.current.style.zIndex = 0;
     }
   }, [scrollPosition])
 
@@ -145,7 +152,7 @@ const Parallax = () => {
           </div>
         </div>
         <div ref={photoPlayer} style={photoPlayerStyle} className={cx(style.photoPlayerMobile, fadeInBottom && style.fadeInBottom, stickyPhoto && style.sticky, scrollPosition >= 415 && style.endScroll)}>
-          <Link href="/portfolio/gallery" className={cx(style.galleryLink, scrollPosition >= 445 && style.centered)}><h3>See All</h3></Link>
+          <Link href="/portfolio/gallery" className={cx(style.galleryLink, scrollPosition >= 535 && style.centered)}><h3>See All</h3></Link>
           <img src="media/kim1.jpeg" ref={mobilePhotoRefs.kim1ref} alt="photo"/>
           <img src="media/angela6.jpg" ref={mobilePhotoRefs.angela6ref} alt="photo" />
           <img src="media/angela5-min.jpg" ref={mobilePhotoRefs.angela5ref} alt="photo" />
@@ -155,7 +162,8 @@ const Parallax = () => {
           <img src="media/nora3-min.jpg" ref={mobilePhotoRefs.nora3ref} alt="photo" />
           <img src="media/angela2.jpg" ref={mobilePhotoRefs.angela2ref} alt="photo" />
           <img src="media/angela3.jpg" ref={mobilePhotoRefs.angela3ref} alt="photo" />
-          <div className={cx(style.overlay, scrollPosition >= 445 && style.endScroll)}></div>
+          <img src="media/nora4.jpg" ref={mobilePhotoRefs.nora4ref} alt="photo" />
+          <div className={cx(style.overlay, scrollPosition >= 535 && style.endScroll)}></div>
         </div>
         <div className={cx(style.photoPlayerDesktop, fadeInBottom && style.fadeInBottom, stickyPhoto && style.sticky)}>
           {/* <img src="media/kim1.jpeg" ref={mobilePhotoRefs.kim1ref} alt="photo"/>
