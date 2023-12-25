844064c2c0569602ac61ccc21f2304363fedad2f
ford burgess <fburg6824@gmail.com>
2023-12-25 23:32:58 +0000

    finished latest articles section

--- components/LatestArticles.js
+++ components/LatestArticles.js
@@ -9,7 +9,7 @@ import { ChevronRight, ChevronLeft } from 'react-feather';
 const Content = (articles) => {
   const content = [];
 
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < 3; i++) {
     content.push(
       <LatestArticle
       blurb={articles[i].content.content[0].content[0].value}
@@ -34,7 +34,9 @@ const LatestArticles = ({ articles }) => {
     item.date = new Date(item.date);
   })
 
-  articles.sort((a, b) => b.date - a.date)
+  articles = articles.sort((a, b) => b.date - a.date).slice(0, 3)
+
+  console.log(articles)
 
   const clickHandler = (e, index) => {
     e.preventDefault();
@@ -54,15 +56,15 @@ const LatestArticles = ({ articles }) => {
     <>
       <div className={styles.container}>
         <Hidden mdDown>
-          <h1 className={styles.header}>Latest Articles</h1>
+          <h1 className={styles.header}>latest articles</h1>
         </Hidden>
-        <Hidden mdUp>
+        {/* <Hidden mdUp>
           <h1 className={styles.header}>LATEST</h1>
         </Hidden>
         <Hidden mdUp>
           <a href="#" onClick={(e) => clickHandler(e, currentArticle - 1)}><ChevronLeft className={styles.chevron} style={{ left: '-5', color: currentArticle == 0 ? "#D0D0D0" : "#000000" }} /></a>
           <a href="#" onClick={(e) => clickHandler(e, currentArticle + 1)}><ChevronRight className={styles.chevron} style={{ right: '-5', color: currentArticle == 3 ? "#D0D0D0" : "#000000" }} /></a>
-        </Hidden>
+        </Hidden> */}
         <div className={styles.articleContainer}>
           {Content(articles)}
         </div>
