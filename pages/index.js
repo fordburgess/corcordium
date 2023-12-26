2e1fb212a4f2c517d4a7c105144a5a61a9a325bf
ford burgess <fburg6824@gmail.com>
2023-12-26 20:19:48 +0000

    mobile landing page done

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,6 @@ import React from 'react'
 import styles from './landingPage.module.css'
 import Image from 'next/image'
 
-const TextContainer = ({ logo }) => {
-  return (
-    <div className={styles.textContainer}>
-      <img src={`/media/${logo}`} alt="logo" className={styles.logo}/>
-      <h1 className={styles.greeting}>hi! welcome to corcordium!</h1>
-    </div>
-  )
-}
-
 const index = () => {
   return (
     <div className={styles.container}>
@@ -18,17 +9,15 @@ const index = () => {
         <video autoPlay muted loop>
           <source src="/media/styling-video.mp4" type="video/mp4" />
         </video>
-        <div className={styles.mobileText}>
-          <img src="/media/logo-small-white.png" alt="logo" className={styles.logo}/>
-          <h1 className={styles.greeting}>hi! welcome to<br /> corcordium!</h1>
+        <div className={styles.mobileGreeting}>
+          <Image className={styles.logoMobile} height={150} width={150} src="/media/logo-small-white.png" alt="logo"/>
+          <h1 className={styles.greetingHeader}>hi! welcome to<br /> corcordium!</h1>
         </div>
-        <div>
-          <Image src="/media/down-arrow-white.png" alt="arrow" height={150} width={150} className={styles.downArrowMobile}/>
-        </div>
+        <Image className={styles.downArrowMobile} height={100} width={70} src="/media/down-arrow-white.png" alt="logo" />
       </div>
       <div className={styles.rightSection}>
         <div className={styles.desktopGreeting}>
-          <Image className={styles.logoDesktop} src="/media/logo-small.png" height={150} width={150}/>
+          <Image className={styles.logoDesktop} src="/media/logo-small.png" height={120} width={120}/>
           <h1 className={styles.greetingHeader}>hi! welcome to<br /> corcordium!</h1>
         </div>
         <Image src="/media/down-arrow.png" alt="arrow" height={150} width={150} className={styles.downArrowDesktop}/>
