06b935ad6f6949b3669d8871cd6cd92865ab6373
ford burgess <fburg6824@gmail.com>
2023-12-29 05:03:24 +0000

    added spicier zine

--- components/ZineDisplay.js
+++ components/ZineDisplay.js
@@ -6,7 +6,7 @@ const ZineDisplay = () => {
     <div className={styles.container}>
       <h1 className={styles.header}>flip through my latest project!</h1>
       <div className={styles.wrapper}>
-        <iframe src="../../pdfFiles/spicier-zine.pdf"></iframe>
+        <iframe style={{ border: 'none', height: '700px', width: '80%' }} src="/pdfFiles/spicier-zine.pdf"></iframe>
       </div>
     </div>
   )
