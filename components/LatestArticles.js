662e4c48c2197eeb728acf05f787218c4720a072
ford burgess <fburg6824@gmail.com>
2024-01-06 23:36:52 +0000

    back scroll works for latest articles

--- components/LatestArticles.js
+++ components/LatestArticles.js
@@ -1,30 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image';
 import styles from './latestarticles.module.css'
-import Link from 'next/link'
 import LatestArticle from './LatestArticle';
-import { Hidden } from '@mui/material';
-import { ChevronRight, ChevronLeft } from 'react-feather';
 
-const Content = (articles) => {
-  const content = [];
-
-  for (var i = 0; i < 3; i++) {
-    content.push(
-      <LatestArticle
-      blurb={articles[i].content.content[1].content[0].value.split(".")[0]}
-      key={articles[i].titlePhoto.sys.id}
-      date={articles[i].date}
-      title={articles[i].title}
-      id={articles[i].titlePhoto.sys.id}
-      index={i}
-      image={articles[i].titlePhoto.fields.file.url}
-      />
-    )
-  }
-
-  return content;
-}
+// const Content = (articles) => {
+//   const content = [];
+//
+//   for (var i = 0; i < 3; i++) {
+//     content.push(
+//       <LatestArticle
+//       blurb={articles[i].content.content[1].content[0].value.split(".")[0]}
+//       key={articles[i].titlePhoto.sys.id}
+//       date={articles[i].date}
+//       title={articles[i].title}
+//       id={articles[i].titlePhoto.sys.id}
+//       index={i}
+//       image={articles[i].titlePhoto.fields.file.url}
+//       />
+//     )
+//   }
+//
+//   return content;
+// }
 
 
 const LatestArticles = ({ articles }) => {
@@ -34,11 +31,10 @@ const LatestArticles = ({ articles }) => {
     item.date = new Date(item.date);
   })
 
-  articles = articles.sort((a, b) => b.date - a.date).slice(0, 3)
+  var articlesSorted = articles.sort((a, b) => b.date - a.date).slice(0, 3)
 
   const scrollToArticle = (index) => {
-
-    if (index > 0 && index < 3) {
+    if (index >= 0 && index <= 2) {
       const container = document.getElementById("article-container");
       const articleWidth = container.clientWidth;
       const newPosition = index * articleWidth;
@@ -52,43 +48,49 @@ const LatestArticles = ({ articles }) => {
     }
   }
 
-  console.log(articles)
-
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>latest articles</h1>
       <div className={styles.articleContainer} id="article-container">
-          {/* {Content(articles)} */}
           {
-            Object.entries(articles).slice(0, 3).map(([key, value], index) => {
+            articlesSorted.slice(0, 3).map((article, index) => {
               return (
-                // <div key={index} style={{ height: "100%", width: "100%", position: 'relative' }}>
-                <>
+                <React.Fragment key={index}>
                   {
-                    index > 0 && (
-                      <Link href="#" onClick={() => scrollToArticle(currentArticle - 1)}>
-                        <Image src="/media/chevron-left.png" height={35} width={35} alt="chevron-left" className={styles.chevronLeft}/>
-                      </Link>
+                    currentArticle > 0 && (
+                      <Image
+                        onClick={() => scrollToArticle(currentArticle - 1)}
+                        src="/media/chevron-left.png"
+                        height={35}
+                        width={35}
+                        alt="chevron-left"
+                        className={styles.chevronLeft}
+                        style={{ zIndex: 999 }}
+                      />
                     )
                   }
-                    <LatestArticle
-                      blurb={value.content.content[1].content[0].value.split(".")[0]}
-                      key={value.titlePhoto.sys.id}
-                      date={value.date}
-                      title={value.title}
-                      id={value.titlePhoto.sys.id}
-                      index={index}
-                      image={value.titlePhoto.fields.file.url}
-                    />
-                    {
-                      index < 2 && (
-                        <Link href="#" onClick={() => scrollToArticle(currentArticle + 1)}>
-                          <Image src="/media/chevron-right.png" height={35} width={35} alt="chevron-right" className={styles.chevronRight}/>
-                        </Link>
-                      )
-                    }
-                  </>
-                // </div>
+                  <LatestArticle
+                    blurb={article.content.content[1].content[0].value.split(".")[0]}
+                    key={article.titlePhoto.sys.id}
+                    date={article.date}
+                    title={article.title}
+                    id={article.titlePhoto.sys.id}
+                    index={index}
+                    image={article.titlePhoto.fields.file.url}
+                  />
+                  {
+                    currentArticle < 2 && (
+                      <Image
+                        onClick={() => scrollToArticle(currentArticle + 1)}
+                        src="/media/chevron-right.png"
+                        height={35}
+                        width={35}
+                        alt="chevron-right"
+                        className={styles.chevronRight}
+                      />
+                    )
+                  }
+                </React.Fragment>
               )
             })
           }
